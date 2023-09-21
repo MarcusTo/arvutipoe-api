cdaaebb200eeb5247ce13781bf8aabf783dc0f93
Opilane TTHK <opilane@tthk.ee>
2023-09-21 11:12:16 +0300

    Added more computers to GetAllComputers  [#186039992]

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
+
 app.get("/computers", (req,res)=>{
     res.send([
-        {id:1,name:"Gaming PC MEGA"},
-        {id:2,name:"Gaming PC Super"}
+        {id:1,name:"Gaming PC Ultra"},
+        {id:2,name:"Gaming PC Mega"},
+        {id:3,name:"Gaming PC Casual"},
+        {id:4,name:"Gaming PC Starter"},
+        {id:5,name:"Gaming PC Budget"},
     ])
 })
 app.listen(port, ()=> {
