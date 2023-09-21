daab599d6292c6661b9656429e2045e7dc17da7e
Opilane TTHK <opilane@tthk.ee>
2023-09-21 10:56:05 +0300

    Add Menu [#186039992]

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json")
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
-
+app.get("/computers", (req,res)=>{
+    res.send([
+        {id:1,name:"Gaming PC MEGA"},
+        {id:2,name:"Gaming PC Super"}
+    ])
+})
 app.listen(port, ()=> {
-    console.log(`API up at: http://localhost${port}`)
+    console.log(`API up at: http://localhost:${port}`)
 })
