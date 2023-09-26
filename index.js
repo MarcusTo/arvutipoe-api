34754d7ef677dddb17edc3b55daa32294c05ec42
MarcusTo <toman.markus12@gmail.com>
2023-09-26 15:51:53 +0300

    Get id from data

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const app = require("express")()
 const port = 8080
 const swaggerui = require("swagger-ui-express")
-const swaggerDocument = require("./docs/swagger.json")
+const swaggerDocument = require("./docs/swagger.yaml")
+const products = require("./products/data")
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
+app.get("/products", (req,res)=>{
+    res.send(products.getAll())
+})
+app.get("/products/:id", (req,res)=>{
+    res.send(products.getById(req.params.id))
+})
 app.listen(port, ()=> {
-    console.log(`API up at: http://localhost${port}`)
+    console.log(`API up at: http://localhost:${port}`)
 })
---
+++ products/data.js
@@ -0,0 +1,15 @@
+let data = [
+    {id:1,name:"Gaming PC Ultra", price: 3999},
+    {id:2,name:"Gaming PC Mega", price: 2450},
+    {id:3,name:"Gaming PC Casual", price: 1999},
+    {id:4,name:"Gaming PC Starter", price: 1450},
+    {id:5,name:"Gaming PC Budget", price: 650},
+]
+
+exports.getAll = ()=> {
+    return data.map(p=> {return {"id": p.id,"name": p.name}})
+}
+
+exports.getById = (id) => {
+    return data
+}
