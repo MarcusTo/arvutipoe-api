6b4608ba8b06b0f6ca733df962cbaee6bd8e29ee
MarcusTo <toman.markus12@gmail.com>
2023-09-28 09:56:10 +0300

    Add getById

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = 8080
 const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.yaml")
 const products = require("./products/data")
+const users = require("./users/data")
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
@@ -10,8 +11,18 @@ app.get("/products", (req,res)=>{
     res.send(products.getAll())
 })
 app.get("/products/:id", (req,res)=>{
-    res.send(products.getById(req.params.id))
+    const foundThing = products.getById(req.params.id)
+    if (foundThing === undefined) {
+       return res.status(404).send({ error: "Widget not found" }) 
+    }
+    res.send(foundThing)
 })
+
+app.get("/products", (req,res)=>{
+    res.send(products.getAll())
+})
+
+
 app.listen(port, ()=> {
     console.log(`API up at: http://localhost:${port}`)
 })
