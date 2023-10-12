89faf15885303646b928c143d05af0a938b6cc1e
MarcusTo <toman.markus12@gmail.com>
2023-10-12 13:39:57 +0300

    app update and delete products endpoints

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-const port = 8080
+const port = process.env.PORT
 const swaggerui = require("swagger-ui-express")
 const yamljs = require("yamljs")
 const swaggerDocument = yamljs.load("./docs/swagger.yaml")
+
 
 app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
@@ -12,5 +14,8 @@ require("./routes/productRoutes")(app)
 require("./routes/userRoutes")(app)
 
 app.listen(port, () => {
+    require("./db").sync()
+        .then(console.log("Synchronized"))
+        .catch((error) => console.log("Error:", error))
     console.log(`API up at: http://localhost:${port}`);
 })
---
+++ models/Game.js
@@ -0,0 +1,17 @@
+module.exports = (dbConnection, Sequelize) => {
+    const Product = dbConnection.define("Product", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        price: {
+            type: Sequelize.DECIMAL
+        }
+    })
+    return Game
+}
