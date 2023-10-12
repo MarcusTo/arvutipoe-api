c63891443ee78df42e797329dd51d465d7cc40b7
Opilane TTHK <opilane@tthk.ee>
2023-10-12 10:21:37 +0300

    connected server to the project.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const app = require("express")()
-const port = 8080
+require("dotenv").config()
+const port = process.env.DB_PORT
 const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.yaml")
 const products = require("./products/data")
 const users = require("./users/data")
+const {Sequelize} = require("sequelize")
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, 
+    process.env.DB_PASS, process.env.DB_HOST,'marcustoman21.thkit.ee ', 'd123442sa470150', 'p92PMBP4TtLtw972bc', {
+    host: 'localhost',
+    dialect: "mariadb"
+  });
+
+  try {
+    await sequelize.authenticate().then (() => {
+        console.log('Connection has been established successfully.');
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 
 app.use("/docs",swaggerui.serve,swaggerui.setup(swaggerDocument))
 
