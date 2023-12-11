baba564e89b13939621cf99826855ad82cc922fc
RasssJ <rasmus.jalakas11@gmail.com>
2023-12-11 22:32:17 +0200

    Create product [#186120823]

---
+++ frontend/components/Product/ProductInfoModal.js
@@ -0,0 +1,92 @@
+import confirmationModal from "../ConfirmationModal.js"
+import productForm from "./ProductForm.js"
+import productDetails from "./ProductDetails.js"
+export default {
+    /*html*/
+    template: `
+<div id="productInfoModal" class="modal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            // <div class="modal-body">
+            // <product-form v-if="isEditing" v-mo v-model:name="modifiedProduct.name" v-model:price="modifiedProduct.price" v-model:productAmount="modifiedProduct.productAmount" :isEditing="true"></product-form>
+            // <product-details v-else v-model:productInModal="productInModal"></product-details>
+            // </div>
+            <div class="modal-footer">
+                <div class="container">
+                    <div class="row">
+                        <template v-if="isEditing">
+                            <div class="col me-auto">
+                                <button type="button" class="btn btn-danger" data-bs-target="#confirmationModal" data-bs-toggle="modal">Delete</button>
+                            </div>
+                            <div class="col-auto">
+                                <button type="button" class="btn btn-success mx-2" @click="saveModifiedProduct">Save</button>
+                                <button type="button" class="btn btn-secondary" @click="cancelEditing">Cancel</button>
+                            </div>
+                        </template>
+                        <template v-else>
+                            <div class="col me-auto"></div>
+                            <div class="col-auto">
+                                <button type="button" class="btn btn-warning mx-2" @click="startEditing">Edit</button>
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </template>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<confirmation-modal :target="'#productInfoModal'" @confirmed="deleteProduct" @canceldelete="cancelEditing"></confirmation-modal>
+
+`,
+    components: {
+        confirmationModal,
+        productForm,
+        productDetails
+    },
+    emits: ["productUpdated"],
+    props: {
+        productInModal: {}
+    },
+    data() {
+        return {
+            isEditing: false,
+            modifiedProduct: {}
+        }
+    },
+    methods: {
+        startEditing() {
+            this.modifiedProduct = { ...this.productInModal }
+            this.isEditing = true
+        },
+        cancelEditing() {
+            this.isEditing = false
+        },
+        async saveModifiedProduct() {
+            console.log("Saving:", this.modifiedProduct);
+            const rawResponse = await fetch(this.API_URL + "/products/" + this.modifiedProduct.id, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.modifiedProduct)
+            });
+            console.log(rawResponse);
+            this.$emit("productUpdated", this.modifiedProduct)
+            this.isEditing = false
+        },
+        deleteProduct() {
+            console.log("Deleting:", this.productInModal);
+            fetch(this.API_URL + "/products/" + this.productInModal.id, {
+                method: 'DELETE'
+            });
+                this.$emit("productUpdated",{})
+                this.isEditing = false
+        }
+        
+    }
+}
---
+++ frontend/views/ProductsView.js
@@ -0,0 +1,69 @@
+import productList from "../components/Product/ProductList.js"
+import productInfoModal from "../components/Product/ProductInfoModal.js"
+import newObjectModal from "../components/NewObjectModal.js"
+import productForm from "../components/Product/ProductForm.js"
+
+export default {
+    /*html*/
+    template: `
+    <button class="btn btn-secondary" @click="newProduct">New Product</button>
+    <product-list :key="update" @showModal="openModal" @deleteProduct="deleteProduct">></product-list>
+    <product-info-modal @Updated="updateView" :productInModal="productInModal"></product-info-modal>
+    <new-object-modal id="newProductModal" @save="saveNewProduct">
+        <product-form v-model:name="productInModal.name" v-model:price="productInModal.price" 
+        v-model:productAmount="productInModal.productAmount"></product-form>
+        <div class="alert alert-danger" role="alert" v-show="error">{{error}}</div>
+    </new-object-modal>
+    `,
+    components: {
+        productList,
+        productInfoModal,
+        newObjectModal,
+        productForm,
+        
+    },
+    data() {
+        return {
+            update: 0,
+            productInModal: { id: "", name: "", price: "",  productAmount: ""},
+            newBarberModal: {},
+            error: ""
+        }
+    },
+    methods: {
+        openModal(product){
+            this.productInModal = product
+            let productInfoModal = new bootstrap.Modal(document.getElementById("productInfoModal"))
+            productInfoModal.show()
+        },
+        // newProduct() {
+        //     this.error = ""
+        //     this.productInModal = {}
+        //     this.newProductModal = new bootstrap.Modal(document.getElementById("newProductModal"))
+        //     this.newProductModal.show()
+        // },
+        updateView(product) {
+            this.update++
+            this.productInModal = product
+        },
+        // async saveNewProduct() {
+        //     console.log("Saving:", this.productInModal)
+        //     const rawResponse = await fetch(this.API_URL + "/products/", {
+        //         method: 'POST',
+        //         headers: {
+        //             'Accept': 'application/json',
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(this.productInModal)
+        //     });
+        //     if (rawResponse.ok) {
+        //         this.newProductModal.hide()
+        //         this.update++
+        //     }
+        //     else {
+        //         const errorResponse = await rawResponse.json()
+        //         this.error = errorResponse.error
+        //     }
+        // }
+    }
+}
