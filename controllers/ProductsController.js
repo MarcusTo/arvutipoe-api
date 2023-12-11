c3f95d24081bf97fd12f90fe087193837f913888
RasssJ <rasmus.jalakas11@gmail.com>
2023-12-11 22:42:11 +0200

    Products List [#186070192]

---
+++ controllers/ProductsController.js
@@ -0,0 +1,51 @@
+const { db } = require("../db")
+const products = db.products
+const { getBaseurl } = require("./helpers")
+
+// CREATE
+exports.createNew = async (req, res) => {
+    if (!req.body.name || !req.body.price || !req.body.productAmount) {
+        return res.status(400).send({ error: "One or all required parameters are missing" })
+    }
+    const createdProduct = await products.create(req.body, {
+        fields: ["name", "price", "productAmount"]
+    })
+    res.status(201)
+        .location(`${getBaseurl(req)}/products/${createdProduct.id}`)
+        .json(createdProduct)
+}
+// READ
+// exports.getAll = async (req, res) => {
+//     const result = await products.findAll({ attributes: ["id", "name","price","productAmount"] })
+//     res.json(result)
+// }
+// exports.getById = async (req, res) => {
+//     const foundProduct = await products.findByPk(req.params.id)
+//     if (foundProduct === null) {
+//         return res.status(404).send({ error: `Product not found`})
+//     }
+//     res.json(foundProduct)
+// }
+// UPDATE
+exports.editById = async (req, res) => {
+    const updateResult = await products.update({ ...req.body }, {
+        where: { id: req.params.id },
+        fields: ["name", "price", "productAmount"]
+    })
+    if (updateResult[0] == 0) {
+        return res.status(404).send({ error: "Product not found" })
+    }
+    res.status(204)
+        .location(`${getBaseurl(req)}/products/${req.params.id}`)
+        .send()
+}
+// DELETE
+exports.deleteById = async (req, res) => {
+    const deletedAmount = await products.destroy({
+        where: { id: req.params.id }
+    })
+    if (deletedAmount === 0) {
+        return res.status(404).send({ error: "Product not found" })
+    }
+    res.status(204).send()
+}
---
+++ frontend/components/Product/ProductList.js
@@ -0,0 +1,38 @@
+// export default {
+//     /*html*/
+//     template: `
+//     <table id="productsTable" class="table table-striped table-bordered">
+//         <thead>
+//             <tr>
+//                 <th>Product ID:</th>
+//                 <th>Product Name:</th>
+//                 <th>Product Price:</th>
+//                 <th>Product Amount:</th>
+//             </tr>
+//         </thead>
+//         <tbody>
+//             <tr v-for="product in products">
+//                 <td @click="getProduct(product.id)">{{ product.id }}</td>
+//                 <td @click="getProduct(product.id)">{{ product.name }}</td>
+//                 <td>{{ product.price }}</td>
+//                 <td>{{ product.productAmount }}</td>
+//             </tr>
+//         </tbody>
+//     </table>
+//     `,
+//     emits: ["showModal"],
+//     data() {
+//         return {
+//             products: []
+//         }
+//     },
+//     async created() {
+//         this.products = await (await fetch("http://localhost:8080/products")).json()
+//     },
+//     methods: {
+//         getProduct: async function (id) {
+//             const productInModal = await (await fetch(this.API_URL + "/products/" + id)).json()
+//             this.$emit("showModal", productInModal)
+//         }
+//     }
+// }
