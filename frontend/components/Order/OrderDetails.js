794c3b6b7469960b23785ca57324dc7af9f9796e
MarcusTo <toman.markus12@gmail.com>
2023-12-12 15:51:21 +0200

    Arvutipood finished.

--- frontend/components/Order/OrderDetails.js
+++ frontend/components/Order/OrderDetails.js
@@ -7,7 +7,6 @@ export default {
             <th>Order ID:</th>
             <td >{{orderInModal.id}}</td>
         </tr>
-
         <tr>
             <th>Product Amount:</th>
             <td >{{orderInModal.productAmount}}</td>
@@ -16,19 +15,11 @@ export default {
             <th>Product ID:</th>
             <td >{{orderInModal.ProductId}}</td>
         </tr>
-        <tr>
-            <th>Product Name:</th>
-            <td >{{orderInModal.Product.name}}</td>
-        </tr>
+       
         <tr>
             <th>User ID:</th>
             <td >{{orderInModal.UserId}}</td>
         </tr>
-        <tr>
-            <th>User Name:</th>
-            <td >{{orderInModal.User.name}}</td>
-        </tr>
-
     </table>
     `,
     props:["orderInModal"]
